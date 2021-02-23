836ecb9d8a143011c14bcb57b187a98c2a8a00c8
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-02-23 21:29:33 +0530

    Update README.md and add IconButton to NavBar.js

--- Client/src/components/Navbar.js
+++ Client/src/components/Navbar.js
@@ -5,16 +5,31 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 import HomeIcon from '@material-ui/icons/Home';
+import { makeStyles } from "@material-ui/core";
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles((theme) => ({
+    typoGraphy: {
+        flexGrow: 1
+    }
+}));
 
 function Navbar(props) {
     const { state } = useContext(UserContext);
+    const classes = useStyles();
     let key = 0;
     const links = () => {
         if (state) {
             return [
                 <Link to='/myfeed' key='myfeed'>MyFeed</Link>,
-                <Link to='/new-post' key='newpost'> New Post</Link>
+                // <Link to='/new-post' key='newpost'>
+                //     <AddIcon />
+                // </Link>
+                <IconButton edge="start" color="inherit" aria-label="menu"  component={Link} to='/new-post'>
+                    <AddIcon />
+                </IconButton>
             ];
         }
 
@@ -31,6 +46,7 @@ function Navbar(props) {
                     <IconButton edge="start" color="inherit" aria-label="menu" className='home-icon' component={Link} to='/'>
                         <HomeIcon />
                     </IconButton>
+                    <Typography className={classes.typoGraphy}></Typography>
                     {
                         links().map(elem => {
                             key++;
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # NodeBook
-This is a social media application. User can sign up, sign in, create post, see posts etc.
+This is a social media application. User can sign up, sign in, create post, see posts etc. Also it provides rich text editing experience to the users using the CKEditor. 
 ## Technology used -
 <ol>
     <li> MERN stack </li>
@@ -8,4 +8,4 @@ This is a social media application. User can sign up, sign in, create post, see 
     <li> JWT token </li>
 </ol>
 
-This is under development
+This is still under development.
